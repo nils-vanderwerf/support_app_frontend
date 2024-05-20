d47d44c28babe5df85069994d188e71726c1bb0f
Nils van der Werf <n.vanderw.92@gmail.com>
2024-05-20 22:29:19 +1000

    Install/Import Bootstrap

--- src/components/ClientList.js
+++ src/components/ClientList.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const ClientList = () => {
   const [clients, setClients] = useState([]);
